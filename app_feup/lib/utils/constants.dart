b967d00f795b5301de4c33437c39895bab98dece
Marco <marco14lyoko@gmail.com>
2022-04-24 17:49:13 +0100

    Created app skeleton

--- app_feup/lib/utils/constants.dart
+++ app_feup/lib/utils/constants.dart
@@ -5,10 +5,20 @@ const navSchedule = 'Horário';
 const navExams = 'Mapa de Exames';
 const navStops = 'Autocarros';
 const navAbout = 'Sobre';
-const navErasmus = 'Erasmus Unite';
 const navBugReport = 'Bugs e Sugestões';
 const navLogOut = 'Terminar sessão';
 
 const faculties = ['faup', 'fbaup', 'fcup', 'fcnaup', 'fadeup',
                   'fdup', 'fep', 'feup', 'ffup', 'flup', 'fmup',
                   'fmdup', 'fpceup', 'icbas'];
+
+// Erasmus Unite Page Constants
+const navErasmusMain = 'Erasmus Unite';
+const navErasmusPaperwork = 'Erasmus Paperwork';
+const navErasmusUniversityPage = 'Erasmus University Page';
+const navErasmusUniversityReview = 'Erasmus University Review';
+const navErasmusMap = 'Erasmus Map';
+const navErasmusAbout = 'Erasmus About';
+const navErasmusStudentsList = 'Erasmus Student List';
+const navErasmusUniversitiesList = 'Erasmus University List';
+const navErasmusReviewList = 'Erasmus Review List';
