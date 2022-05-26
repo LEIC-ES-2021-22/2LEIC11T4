57d8b49e646d22503f129a63659f465665c0f35d
matildesequeira <up202007623@edu.fe.up.pt>
2022-05-26 18:44:02 +0100

    See reviews page

--- app_feup/lib/view/Widgets/Erasmus/erasmus_nav_card.dart
+++ app_feup/lib/view/Widgets/Erasmus/erasmus_nav_card.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:uni/controller/local_storage/image_offline_storage.dart';
+import 'package:uni/model/app_state.dart';
 
 import 'package:uni/model/erasmus/universityItem.dart';
+import 'package:uni/view/Widgets/Erasmus/star_evaluation_view.dart';
+import '../../../model/erasmus/universityReview.dart';
 import '../../Pages/Erasmus/erasmus_uni_page_view.dart';
 import '../generic_card.dart';
 
@@ -84,3 +90,62 @@ class ErasmusUniversityCard extends GenericCard {
     ErasmusUniversityPageViewState.university = this.uni;
   }
 }
+
+
+class ErasmusReviewCard extends GenericCard {
+  final UniversityReview review;
+  final gotoPage;
+
+  ErasmusReviewCard(this.review, this.gotoPage);
+
+  ErasmusReviewCard.fromEditingInformation(
+      Key key, bool editingMode, Function onDelete, this.review, this.gotoPage)
+      : super.fromEditingInformation(key, editingMode, onDelete);
+
+  @override
+  Widget buildCardContent(BuildContext context) {
+    final MediaQueryData queryData = MediaQuery.of(context);
+
+    return Row(children: [
+      Container(
+          width: queryData.size.height / 7,
+          height: queryData.size.height / 7,
+          child: Image.network('https://sigarra.up.pt/feup/pt/fotografias_service.foto?pct_cod=up' + review.studentID)),
+      SizedBox(width: 30),
+      Expanded(
+        child: Column(
+          children: [
+            Text(review.text,
+                softWrap: true, style: Theme.of(context).textTheme.headline2),
+            SizedBox(height: 10),
+
+            StarDisplay(value: review.stars.experience),
+            SizedBox(height: 5)
+          ],
+        ),
+      ),
+    ]);
+  }
+
+  @override
+  String getTitle() => this.review.studentID;
+
+  @override
+  onClick(BuildContext context) {
+  }
+
+}
+
+Future<File> loadProfilePic(String studentno) {
+  final String studentNo = studentno;
+  String url =
+      'https://sigarra.up.pt/feup/pt/fotografias_service.foto?pct_cod=';
+  final Map<String, String> headers = Map<String, String>();
+
+  if (studentNo != null) {
+    url += studentNo;
+    headers['cookie'] = studentno;
+  }
+  return retrieveImage(url, headers);
+}
+
